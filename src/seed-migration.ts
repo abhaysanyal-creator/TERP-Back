88ee49eb5fc9ef4ebbb5a1d8a84338f4f031133e
abhaysanyal59 <abhaysanyal59@gmail.com>
2025-10-27 12:07:40 +0530

    Refined seeding

--- src/seed-migration.ts
+++ src/seed-migration.ts
@@ -14,33 +14,26 @@ async function seedSuperAdmin() {
     await mongoose.connect(uri);
     console.log("📡 Connected to MongoDB");
 
-    const role = await Role.findOne({ name: "super-admin" }); // or { code: "SUPER_ADMIN" } etc.
+    const role = await Role.findOne({ name: "super-admin" });
     if (!role) {
-      console.error(
-        "❌ Role 'Super Admin' not found. Please seed roles first."
-      );
+      console.error("Role 'super-admin' not found. Please seed roles first.");
+
+      const superAdminRole = await Role.create({
+        name: "super-admin",
+        permissions: ["*"], // full access
+      });
+      console.log(`Seeded Role - ${superAdminRole}`);
       process.exit(1);
     }
 
-    console.log({
-      username: "abhay_sanyal",
-      contact_number: 8053915959,
-      name: "Abhay Sanyal",
-      email: "abhay.sanyal@rampwin.com",
-      // password: hashedPassword,
-      role: role._id,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
     const exists = await User.findOne({ email: "abhay.sanyal@rampwin.com" });
     if (exists) {
       console.log("Super Admin already exists");
       return;
     }
-    
+
     const hashedPassword = await bcrypt.hash("terp_admin123", 10);
-    
-  
+
     await User.create({
       username: "abhay_sanyal",
       contact_number: 8053915959,
