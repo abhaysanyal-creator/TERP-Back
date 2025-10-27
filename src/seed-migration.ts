6adeb523df6016b88091e7bc80b0040c251a54a0
abhaysanyal59 <abhaysanyal59@gmail.com>
2025-10-27 11:59:30 +0530

    Migrations

---
+++ src/seed-migration.ts
@@ -0,0 +1,64 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import User from "./models/user.model"; // adjust path if needed
+import Role from "./models/role.model";
+
+const uri = process.env.DATABASE_URI as string;
+
+async function seedSuperAdmin() {
+  console.log("Enter Seeding Script");
+  try {
+    await mongoose.connect(uri);
+    console.log("📡 Connected to MongoDB");
+
+    const role = await Role.findOne({ name: "super-admin" }); // or { code: "SUPER_ADMIN" } etc.
+    if (!role) {
+      console.error(
+        "❌ Role 'Super Admin' not found. Please seed roles first."
+      );
+      process.exit(1);
+    }
+
+    console.log({
+      username: "abhay_sanyal",
+      contact_number: 8053915959,
+      name: "Abhay Sanyal",
+      email: "abhay.sanyal@rampwin.com",
+      // password: hashedPassword,
+      role: role._id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+    const exists = await User.findOne({ email: "abhay.sanyal@rampwin.com" });
+    if (exists) {
+      console.log("Super Admin already exists");
+      return;
+    }
+    
+    const hashedPassword = await bcrypt.hash("terp_admin123", 10);
+    
+  
+    await User.create({
+      username: "abhay_sanyal",
+      contact_number: 8053915959,
+      name: "Abhay Sanyal",
+      email: "abhay.sanyal@rampwin.com",
+      password: hashedPassword,
+      role: role._id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    console.log("✅ Super Admin created successfully");
+  } catch (err) {
+    console.error("❌ Error during seeding:", err);
+  } finally {
+    await mongoose.disconnect();
+    console.log("🔌 Disconnected from MongoDB");
+  }
+}
+
+seedSuperAdmin();
