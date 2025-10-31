661c6d7c443661389a8a4c7a469fc9779960efc4
abhaysanyal59 <abhaysanyal59@gmail.com>
2025-10-31 19:37:13 +0530

    FIX1

--- migrations/20251031134100-db-data.cjs
+++ migrations/20251031134100-db-data.cjs
@@ -1,9 +1,25 @@
 module.exports = {
   async up(db, client) {
-    console.log("================ END ==================");
+    console.log("Fetching roles...");
+
+    const roles = await db.collection("roles").find({}).toArray();
+
+    if (!roles.length) {
+      console.log("No roles found in DB");
+      return;
+    }
+
+    console.log("---- ROLES ----");
+    roles.forEach((r) => {
+      console.log(`ID: ${r._id} | NAME: ${r.name}`);
+    });
+    console.log("---------------");
   },
 
   async down(db, client) {
-    console.log("No rollback for viewing roles");
+    console.log("Nothing to rollback");
   },
 };
+
+
+
