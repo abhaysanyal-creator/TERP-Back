3d2de1eb28ba622ba5ab68aed0aa0d2ea0d06fd1
abhaysanyal59 <abhaysanyal59@gmail.com>
2025-10-31 19:26:26 +0530

    FIX

---
+++ migrations/20251031130838-seed-perm-sup_admin.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  async up(db, client) {
+    console.log("Fetching roles...");
+
+    const roles = await db.collection("roles").find({}).toArray();
+
+    if (!roles.length) {
+      console.log("No roles found in DB");
+      return;
+    }
+
+    console.log("---- ROLES ----");
+    roles.forEach((r) => {
+      console.log(`ID: ${r._id} | NAME: ${r.name}`);
+    });
+    console.log("---------------");
+  },
+
+  async down(db, client) {
+    console.log("Nothing to rollback");
+  },
+};
--- migrations/20251031134100-db-data.cjs
+++ migrations/20251031134100-db-data.cjs
@@ -1,14 +1,5 @@
 module.exports = {
   async up(db, client) {
-    const roles = await db
-      .collection("roles")
-      .find({}, { projection: { name: 1 } })
-      .toArray();
-
-    console.log("================== ROLES IN DB ====================");
-    roles.forEach((r) => {
-      console.log(`_id: ${r._id}, name: ${r.name}`);
-    });
     console.log("================ END ==================");
   },
 
