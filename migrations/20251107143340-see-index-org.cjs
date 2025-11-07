8849fed4c3644aaf14fa558dc1f49c837ccf6490
abhaysanyal59 <abhaysanyal59@gmail.com>
2025-11-07 20:03:54 +0530

    Index Org

---
+++ migrations/20251107143340-see-index-org.cjs
@@ -0,0 +1,28 @@
+// migrations/20251107121000-show-collection-indexes.js
+
+module.exports = {
+  async up(db, client) {
+    console.log("🔍 Fetching indexes from 'activities' collection...");
+
+    const indexes = await db.collection("activities").indexes();
+
+    console.log("📚 Existing Indexes:");
+    indexes.forEach((index) => {
+      console.log("----------------------------------------------------");
+      console.log("Name:", index.name);
+      console.log("Keys:", JSON.stringify(index.key));
+      if (index.unique) console.log("Unique:", index.unique);
+      if (index.partialFilterExpression)
+        console.log(
+          "Partial Filter:",
+          JSON.stringify(index.partialFilterExpression)
+        );
+    });
+
+    console.log("✅ Done listing indexes.\n");
+  },
+
+  async down(db, client) {
+    // no rollback needed
+  },
+};
