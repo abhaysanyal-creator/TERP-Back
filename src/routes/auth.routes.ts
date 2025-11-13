3d0891e05e73854ac2f26e5222fc2f00f52c3e73
abhaysanyal59 <abhaysanyal59@gmail.com>
2025-11-13 16:02:05 +0530

    Router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { loginValidator } from "../validators/auth.validator";
 import {
+  getMeController,
   loginController,
   verifyOtpController,
 } from "../controllers/auth.controller";
+import { authorisationMiddleware } from "../middlewares/auth.middlewares";
 
 const router = Router();
 
 router.post("/login", loginValidator, loginController);
 router.post("/verify-otp", verifyOtpController);
+router.get("/me",authorisationMiddleware,getMeController)
 
 export default router;
