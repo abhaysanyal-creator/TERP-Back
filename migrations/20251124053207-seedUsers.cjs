a60793d969542c03e8d9a9d50cb428edb91d6b96
Abhay <abhay.sanyal@rampwin.com>
2025-11-24 12:10:40 +0530

    Seed

---
+++ migrations/20251124053207-seedUsers.cjs
@@ -0,0 +1,7 @@
+export const up = async () => {
+  console.log("Skipping bad migration 20251124053207-seedUsers.cjs");
+};
+
+export const down = async () => {
+  console.log("Skipping down migration.");
+};
